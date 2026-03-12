e31611580b06b3a9cb09d2170faa76617645a3f9
YuanShuoHsu <ook1725@gmail.com>
2026-03-12 17:31:17 +0800

    feat: Add `@mui/x-data-grid` localization to locale configurations.

--- constants/locale.ts
+++ constants/locale.ts
@@ -3,6 +3,7 @@ import { LocaleEnum } from "@/enums/Locale";
 import type { Locale } from "@/i18n/routing";
 
 import * as muiLocales from "@mui/material/locale";
+import * as dataGridLocales from "@mui/x-data-grid/locales";
 import * as pickersLocales from "@mui/x-date-pickers/locales";
 
 import type { LocaleConfig } from "@/types/locale";
@@ -13,34 +14,34 @@ export const locales: Record<Locale, LocaleConfig> = {
     dayjs: "zh-tw",
     ecpay: "",
     label: "繁體中文",
-    mui: [muiLocales.zhTW, pickersLocales.zhTW],
+    mui: [dataGridLocales.zhTW, muiLocales.zhTW, pickersLocales.zhTW],
   },
   [LocaleEnum.En]: {
     countryCode: "US",
     dayjs: "en",
     ecpay: "ENG",
     label: "English",
-    mui: [muiLocales.enUS, pickersLocales.enUS],
+    mui: [dataGridLocales.enUS, muiLocales.enUS, pickersLocales.enUS],
   },
   [LocaleEnum.Ja]: {
     countryCode: "JP",
     dayjs: "ja",
     ecpay: "JPN",
     label: "日本語",
-    mui: [muiLocales.jaJP, pickersLocales.jaJP],
+    mui: [dataGridLocales.jaJP, muiLocales.jaJP, pickersLocales.jaJP],
   },
   [LocaleEnum.Ko]: {
     countryCode: "KR",
     dayjs: "ko",
     ecpay: "KOR",
     label: "한국어",
-    mui: [muiLocales.koKR, pickersLocales.koKR],
+    mui: [dataGridLocales.koKR, muiLocales.koKR, pickersLocales.koKR],
   },
   [LocaleEnum.ZhCN]: {
     countryCode: "CN",
     dayjs: "zh-cn",
     ecpay: "CHI",
     label: "简体中文",
-    mui: [muiLocales.zhCN, pickersLocales.zhCN],
+    mui: [dataGridLocales.zhCN, muiLocales.zhCN, pickersLocales.zhCN],
   },
 };
