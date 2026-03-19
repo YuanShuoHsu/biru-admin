2d956a96f557108c6ca1fd27a78d8b1f2fa83396
YuanShuoHsu <ook1725@gmail.com>
2026-03-19 14:09:42 +0800

    style: reformat Button component attributes in verify email page.

--- app/[locale]/(app)/(dashboard)/auth/verify-email/index.tsx
+++ app/[locale]/(app)/(dashboard)/auth/verify-email/index.tsx
@@ -243,7 +243,14 @@ const AuthVerifyEmail = ({
     },
     [VERIFY_STATUS.VERIFYING]: {
       actions: (
-        <Button disabled fullWidth loading loadingPosition="end" size="large" variant="contained">
+        <Button
+          disabled
+          fullWidth
+          loading
+          loadingPosition="end"
+          size="large"
+          variant="contained"
+        >
           {verifyingTitle}
         </Button>
       ),
