1da2e59d50054a0e7297bbbb8d806b0490377521
許元碩 <ook1725@gmail.com>
2026-04-25 21:30:11 +0800

    refactor: remove Passkeys component from security settings page

--- app/[locale]/(app)/(dashboard)/auth/settings/security/index.tsx
+++ app/[locale]/(app)/(dashboard)/auth/settings/security/index.tsx
@@ -1,14 +1,12 @@
 import ChangePassword from "./ChangePassword";
 import Danger from "./Danger";
 import LinkedAccounts from "./LinkedAccounts";
-import Passkeys from "./Passkeys";
 import Sessions from "./Sessions";
 
 const Security = () => (
   <>
     <ChangePassword />
     <LinkedAccounts />
-    <Passkeys />
     <Sessions />
     <Danger />
   </>
